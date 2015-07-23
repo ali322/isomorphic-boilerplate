8e9ff526bb5c814abedebed49992318c093fdeff
alichen <ali322@gmail.com>
2015-07-23 18:22:22 +0800

    change

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var express = require("express"),
-    http = require("http"),
-    bodyParser = require("body-parser"),
-    methodOverride = require("method-override"),
-    cons = require("consolidate");
+var express = require("express");
+    // http = require("http"),
+    // bodyParser = require("body-parser"),
+    // methodOverride = require("method-override"),
+    // cons = require("consolidate");
 
 var app = express();
 
 var router = require("./router.js");
 
 app.use(express.static('public'));
-app.use(bodyParser());
-app.use(methodOverride());
-app.engine('html', cons.swig);
-app.set('view engine', 'html');
-app.set("views", __dirname + '/view');
+// app.use(bodyParser());
+// app.use(methodOverride());
+// app.engine('html', cons.swig);
+// app.set('view engine', 'html');
+// app.set("views", __dirname + '/view');
 // app.use(router);
 app.get("/",function(req,res){
     res.send("hello!");
