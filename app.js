ec54d6e194dbfec96641a8b6c61fbcd74d6c1024
alichen <ali322@gmail.com>
2015-07-24 10:49:30 +0800

    change app.js

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 var express = require("express");
     // http = require("http"),
-    // bodyParser = require("body-parser"),
-    // methodOverride = require("method-override"),
-    // cons = require("consolidate");
+    bodyParser = require("body-parser"),
+    methodOverride = require("method-override"),
+    cons = require("consolidate");
 
 var app = express();
 
 var router = require("./router.js");
 
 app.use(express.static('public'));
-// app.use(bodyParser());
-// app.use(methodOverride());
-// app.engine('html', cons.swig);
-// app.set('view engine', 'html');
-// app.set("views", __dirname + '/view');
-// app.use(router);
-app.get("/",function(req,res){
-    res.send("hello!");
-});
+app.use(bodyParser());
+app.use(methodOverride());
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set("views", __dirname + '/view');
+app.use(router);
 
 app.listen(8081, function() {
     console.log("hyena start at %d", 8081);
