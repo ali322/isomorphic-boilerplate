cccc2111cc58b34ceb380319334106b4f92ea046
alichen <ali322@gmail.com>
2015-08-13 18:02:40 +0800

    add test in both of client and server

---
+++ server/__test__/server-test.js
@@ -0,0 +1,9 @@
+var chai = require("chai"),
+    expect = chai.expect;
+
+describe("sum", function() {
+    it("1+1 should equal to 2", function() {
+        var GoodList = require("../list/list.jsx");
+        expect(1 + 1).to.equal(2);
+    })
+});
