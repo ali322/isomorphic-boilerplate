bb4c00df4b50d94f2e034887ea2afd0e6289978a
alichen <ali322@gmail.com>
2015-08-20 23:16:18 +0800

    change to initialState

--- server/ctrl/main.js
+++ server/ctrl/main.js
@@ -4,21 +4,21 @@ var React = require("react");
 var IndexComponent = React.createFactory(require("../../shared/index/component.jsx"));
 
 let index = function(req, res) {
-    var initialData = {
+    var initialState = {
         resFetched: true,
         pagination: {
             count: 6
         }
     };
     var markup = React.renderToString(IndexComponent({
-        initialData
+        initialState
     }));
     res.render("index", {
         //base:'view/index.html',
         //isNonStatic:false,
         //tags: 'content',
         content: markup,
-        initialData
+        initialState
     });
 };
 
