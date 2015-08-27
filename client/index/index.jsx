e59df609f71a2ce06ba0e21c3cadb24ccde41773
alichen <ali322@gmail.com>
2015-08-27 14:28:46 +0800

    change client bootstrap

--- client/index/index.jsx
+++ client/index/index.jsx
@@ -1,9 +1,13 @@
 'use strict'
 import IndexComponent from "../../shared/index/component.jsx";
 
-if(typeof window !== 'undefined'){
-    document.addEventListener('DOMContentLoaded',function(){
-        var initialData = JSON.parse(document.getElementById("initial-data").textContent);
-        React.render(<IndexComponent initialData={initialData} />,document.body);
-    });
+function bootstrap(){
+    var initialState = JSON.parse(document.getElementById("initial-state").textContent);
+        React.render(<GoodList initialState={initialState} />,document.body);
+}
+
+if(typeof window.addEventListener){
+    window.addEventListener("DOMContentLoaded",bootstrap);
+}else{
+    window.attachEvent('onload',bootstrap);
 }
