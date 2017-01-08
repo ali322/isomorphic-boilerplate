1da18771a8a9c0b2c3a3d1df1636b35d21f8df27
alichen <ali322@gmail.com>
2017-01-09 00:16:42 +0800

    fix __tests__

--- __tests__/client/index/component.js
+++ __tests__/client/index/component.js
@@ -3,36 +3,36 @@
 import test from "ava"
 import sinon from "sinon"
 import React from "react";
-import {shallow,mount} from "enzyme"
+import { shallow, mount } from "enzyme"
 import Weather from "../../../shared/chunk/index/component.jsx";
-import initialState from "./initialState"
+import initialState from "./initialstate.json"
 
-let wrapper,el
+let wrapper, el
 
-test.before(t=>{
+test.before(t => {
     const props = {
-        changeFiwrapperd:sinon.spy(),
-        fetchWeather:sinon.spy(),
-        handleChange:sinon.spy(),
-        weatherByCityName:initialState
+        changeFiwrapperd: sinon.spy(),
+        fetchWeather: sinon.spy(),
+        handleChange: sinon.spy(),
+        weatherByCityName: initialState
     }
     wrapper = shallow(<Weather {...props}/>)
     el = wrapper.instance()
 })
 
-test("should render correct",t=>{
-    t.is(wrapper.find("input").prop("value"),"长沙")
-    t.is(wrapper.find("p").first().children().last().text(),"changsha")
+test("should render correct", t => {
+    t.is(wrapper.find("input").prop("value"), "长沙")
+    t.is(wrapper.find("p").first().children().last().text(), "changsha")
 })
 
-test.skip("should call handleChange once after change value",t=>{
+test.skip("should call handleChange once after change value", t => {
     const input = wrapper.find("input")
-    input.simulate("change",{target:{value:"上海"},preventDefault:()=>{}})
-    t.is(el.props.handleChange.callCount,1)
+    input.simulate("change", { target: { value: "上海" }, preventDefault: () => {} })
+    t.is(el.props.handleChange.callCount, 1)
 })
 
-test("should call handleQuery once after click",t=>{
+test("should call handleQuery once after click", t => {
     const button = wrapper.find("button")
     button.simulate("click")
-    t.is(el.props.fetchWeather.callCount,1)
+    t.is(el.props.fetchWeather.callCount, 1)
 })
