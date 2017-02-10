33e55fd1c25e292a06334cb076aa6d28db377df4
alichen <ali322@gmail.com>
2017-02-11 00:51:36 +0800

    fix server test

--- __tests__/server/index.js
+++ __tests__/server/index.js
@@ -1,8 +1,7 @@
-'use strict';
 import nock from "nock"
 import test from "ava"
 
-test("index route should response correct",t=>{
-    // nock(":3000").get("/").reply(200)
-    t.is(1+1,2)
+test("index route should response correct", t => {
+    nock(":3000").get("/").reply(200)
+    t.is(1 + 1, 2)
 })
