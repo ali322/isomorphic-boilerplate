fd5998c63346d34d5073f75587bad1146639b2bc
alichen <ali322@gmail.com>
2017-05-03 15:04:58 +0800

    finish test

--- client/test/e2e/fixture/server.js
+++ client/test/e2e/fixture/server.js
@@ -1,5 +1,7 @@
 let http = require('http')
-let app = require('../../../server/dist/bootstrap')
+// let app = require('../../../../server/dist/bootstrap')
+let Koa = require('koa')
+let app = new Koa()
 
 let server = http.createServer(app.callback()).listen(3000)
 
---
+++ client/test/unit/fixture/conf.js
@@ -0,0 +1,27 @@
+let { resolve } = require('path')
+
+let entry = resolve('client', 'test', 'unit', 'fixture', 'setup.js')
+let reportFolder = resolve('client', 'test', 'unit', 'coverage')
+
+let webpackConfig = {
+    module: {
+        rules: [{
+            test: /\.(js|es6|jsx)/,
+            loader: 'babel-loader',
+            include: [
+                resolve('client')
+            ]
+        }]
+    },
+    resolve: {
+        alias: {
+            "@": resolve('client', 'bundle')
+        }
+    }
+}
+
+module.exports = {
+    entry,
+    reportFolder,
+    webpack: webpackConfig
+}
