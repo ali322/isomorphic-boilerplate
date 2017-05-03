27c364a50c254f3581dbc39c8f28e45216e66bce
alichen <ali322@gmail.com>
2017-05-03 16:03:49 +0800

    fix pass

--- client/test/e2e/fixture/server.js
+++ client/test/e2e/fixture/server.js
@@ -1,7 +1,5 @@
 let http = require('http')
-// let app = require('../../../../server/dist/bootstrap')
-let Koa = require('koa')
-let app = new Koa()
+let app = require('../../../../server/dist/bootstrap')
 
 let server = http.createServer(app.callback()).listen(3000)
 
