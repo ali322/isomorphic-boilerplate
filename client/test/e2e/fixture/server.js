23c835d021bd1289a4eccfe3fb75b456ae44c5f6
alichen <ali322@gmail.com>
2017-06-22 17:21:32 +0800

    fix pass

--- client/test/e2e/fixture/server.js
+++ client/test/e2e/fixture/server.js
@@ -2,7 +2,7 @@ let app = require('../../../../dist/server/bootstrap')
 
 let server = require('nva-server')({
     log: false,
-    mock: require('../../../../.nva/mock/event')
+    mock: '.nva/mock/event'
 })
 
 server.use(app.callback())
