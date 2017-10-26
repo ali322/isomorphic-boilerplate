3fb657880b58ca6e2d7c2eb46cea0bd97e81a82f
alichen <ali322@gmail.com>
2017-10-26 11:39:46 +0800

    switch to jest

--- client/test/unit/spec/index.js
+++ client/test/unit/spec/index.js
@@ -1,64 +1,71 @@
-import Vue from 'vue'
+import 'vue'
+import Vuex from 'vuex'
+import { shallow, createLocalVue } from 'vue-test-utils'
 import moxios from 'moxios'
-import sinon from 'sinon'
 import mutations from '@/index/module/mutation'
 import * as actions from '@/index/module/action'
 import * as constants from '@/index/module/constant'
 import app from '@/index/module/app.vue'
-import { testAction, renderedText, createVM, destoryVM, triggerEvent } from '../fixture/util'
+import { testAction } from '../helper'
 
-describe("mutations", () => {
-    it(constants.RESPONSE_EVENTS, () => {
-        let state = {}
-        mutations[constants.RESPONSE_EVENTS](state)
-        expect(state.eventsFetched).to.equal(true)
-    })
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+describe('mutations', () => {
+  it(constants.RESPONSE_EVENTS, () => {
+    let state = {}
+    mutations[constants.RESPONSE_EVENTS](state)
+    expect(state.eventsFetched).toBe(true)
+  })
 })
 
-describe("actions", () => {
-    beforeEach(() => {
-        moxios.install()
-    })
-    afterEach(() => {
-        moxios.uninstall()
-    })
-    it('should RESPONSE_EVENTS when fetched', done => {
-        let ret = {
-            status: 'ok',
-            data: []
-        }
-        let expectedMutations = [
-            { type: constants.REQUEST_EVENTS },
-            { type: constants.RESPONSE_EVENTS, payload: ret.data }
-        ]
+describe('actions', () => {
+  beforeEach(() => {
+    moxios.install()
+  })
+  afterEach(() => {
+    moxios.uninstall()
+  })
+  it('should RESPONSE_EVENTS when fetched', done => {
+    let ret = {
+      status: 'ok',
+      data: []
+    }
+    let expectedMutations = [
+      { type: constants.REQUEST_EVENTS },
+      { type: constants.RESPONSE_EVENTS, payload: ret.data }
+    ]
 
-        moxios.stubRequest('/mock/events', {
-            status: 200,
-            responseText: JSON.stringify(ret)
-        })
-
-        testAction(actions.fetchEvents, [], {}, actions, expectedMutations, done)
+    moxios.stubRequest('/mock/events', {
+      status: 200,
+      responseText: JSON.stringify(ret)
     })
+
+    testAction(actions.fetchEvents, [], {}, actions, expectedMutations, done)
+  })
 })
 
-describe("component", () => {
-    let vm, fetchEvents
-    beforeEach(() => {
-        fetchEvents = sinon.spy()
-        vm = createVM({ ...app, methods: { ...app.methods, fetchEvents }, computed: {} })
+describe('component', () => {
+  let wrapper, store, fetchEvents
+  beforeEach(() => {
+    fetchEvents = jest.fn()
+    store = new Vuex.Store({
+      state: { index: { events: [] } },
+      actions: { fetchEvents }
     })
-    afterEach(() => {
-        destoryVM(vm)
+    wrapper = shallow(app, {
+      localVue,
+      store
     })
-    it('should render correct', () => {
-        expect(vm.$el.querySelectorAll('.content').length).to.equal(1)
-    })
-    it('should call handleRefresh once after click', done => {
-        const button = vm.$el.querySelector('.refresh-btn')
-        triggerEvent(button, 'click')
-        vm.$nextTick(() => {
-            expect(fetchEvents.callCount).to.equal(1)
-            done()
-        })
-    })
+    wrapper.setMethods({ fetchEvents })
+  })
+  it('should render correct', () => {
+    expect(wrapper.findAll('.content').length).toBe(1)
+  })
+  it('should call handleRefresh once after click', () => {
+    const button = wrapper.find('.refresh-btn')
+    button.trigger('click')
+    expect(fetchEvents).toHaveBeenCalledTimes(1)
+  })
 })
